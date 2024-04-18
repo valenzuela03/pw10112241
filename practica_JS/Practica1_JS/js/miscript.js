42ea9c1684061a1679350acf065f4554618ad62f
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-04-18 11:00:48 -0700

    Practicando JS

---
+++ practica_JS/Practica1_JS/js/miscript.js
@@ -0,0 +1,10 @@
+var a = 0;
+        let b = 0; //scope - alcance - ambito//
+        const pi = 3.1416;
+        alert("JS en proceso");
+        function boton(){
+            alert("Clic");
+        }
+        const flecha = ()=>{ //arrow function
+            alert("Funcion flecha - anonima")
+        }
