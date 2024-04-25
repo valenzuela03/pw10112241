25b081b50b6c39e226b6adf160649751cd73503b
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-04-25 10:53:33 -0700

    Conexion a MySQL

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,13 +6,42 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a MySQL
+conexion.connect(function (error) {
+    if (error) {
+        throw error;
+    } else {
+        console.log('Conectado a la base de datos')
+    }
+});
+
 //Rutas de acceso
-app.get("/", function(req,res){
-res.send("Ruta de inicio")
+app.get("/", function (req, res) {
+    res.send("Ruta de inicio")
+})
+
+//Seleccionamos todos losclientes
+app.get('/api/clientes', (req,res)=>{
+    conexion.query('SELECT * FROM clientes',(error, filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
 })
 
 //Encender servidor (Todos los servidores se prenden en el puerto 80 y uno seguro es el 4443)
 let puerto = 3000;
-app.listen(puerto, function(){
-    console.log("Servidor escuchando puerto "+puerto);
+app.listen(puerto, function () {
+    console.log("Servidor escuchando puerto " + puerto);
 })
