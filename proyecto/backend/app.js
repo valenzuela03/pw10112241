d7c1dd78dbfaac8cf2c23669ea13272a501b33bb
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-05-02 10:59:29 -0700

    Backend Terminado

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -69,6 +69,7 @@ app.post('/api/clientes/', (req, res) => {
         id:req.body.id,
         nombre:req.body.nombre,
         direccion:req.body.direccion,
+        telefono:req.body.telefono,
         rfc:req.body.rfc
     }
     let sql = "INSERT INTO clientes SET ?";
@@ -77,6 +78,23 @@ app.post('/api/clientes/', (req, res) => {
             throw error;
         } else {
             res.send(resultado);
+        }
+    })
+});
+
+//ACTUALIZAR DATOS
+app.put('/api/clientes/:id', (req, res) =>{
+    let id = req.params.id;
+    let nombre = req.body.nombre;
+    let direccion = req.body.direccion;
+    let telefono = req.body.telefono;
+    let rfc = req.body.rfc;
+    let sql = "UPDATE clientes SET nombre=?, direccion=?, telefono=?, rfc=? WHERE id=?";
+    conexion.query(sql,[nombre, direccion, telefono, rfc, id],(error, results) => {
+        if(error){
+            throw error;
+        } else {
+            res.send(results)
         }
     })
 });
