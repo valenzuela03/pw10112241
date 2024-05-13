01f750933887bd093e40c98369759e89fe96e9b2
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-05-13 11:03:07 -0700

    Frontend Lista Clientes

---
+++ tienda/src/views/ClientesView.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Clientes</h4>
+                <RouterLink to="/clientes/create" class="btn btn-primary float-end">
+                    Agregar
+                </RouterLink>
+            </div>
+            <div class="card-body">
+                <table class="table table-borderered table-striped">
+                    <thead>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Direccion</th>
+                        <th>Telefono</th>
+                        <th>RFC</th>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(clientes, index) in clientes" :key="index">
+                            <td>{{ clientes.id }}</td>
+                            <td>{{ clientes.nombre }}</td>
+                            <td>{{ clientes.direccion }}</td>
+                            <td>{{ clientes.telefono }}</td>
+                            <td>{{ clientes.rfc }}</td>
+                            <td>Editar &nbsp; Borrar</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import axios from 'axios';
+    export default {
+        name: "ClientesView",
+        data(){
+            return{ 
+                clientes: [],
+            }
+        },
+        mounted(){ //cuando se carga la pagina
+            this.getClientes();
+        },
+        methods: {
+            getClientes(){
+                axios.get('http://localhost:3000/api/clientes').then(res => {
+                    this.clientes = res.data;
+                });
+            }
+        }
+    }
+</script>
