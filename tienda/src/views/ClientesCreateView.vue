c66494fe9bd443fb7ae47f70a9d45ed6f53a446a
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-05-16 10:48:56 -0700

    Mensaje Agregado con exito

--- tienda/src/views/ClientesCreateView.vue
+++ tienda/src/views/ClientesCreateView.vue
@@ -2,6 +2,9 @@
     <div class="container mt-5">
         <div class="card">
             <h4>Agregar clientes</h4>
+             <div v-if="mensaje == 1" class="alert alert-success" role="alert">  <!-- MENSAJE DE EXITO -->
+                Datos guardados con exito
+            </div>
         </div>
         <div class="card-body">
             <div class="mb-3">
@@ -25,17 +28,19 @@
                 <input type="text" class="form-control" v-model="model.cliente.rfc">
             </div>
             <div class="mb-3">
-                <button class="btn btn-primary">Guardar</button>
+                <button class="btn btn-primary" @click="guardarClientes()">Guardar</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
     export default {
         name: "ClientesCreate",
         data(){
             return{
+                mensaje: 0, //para mostrar mensaje de exito
                 model:{
                     cliente:{
                     id: '',
@@ -45,6 +50,25 @@
                     RFC: ''
                 }
                 }
+            }
+        },
+        methods:{
+            guardarClientes(){
+                axios.post('http://localhost:3000/api/clientes', this.model.cliente)
+                .then(res => {
+                    //si insertamos 1 registro
+                    if(res.data.affectedRows == 1){ 
+                        //limpiamos los cuadros de texto
+                        this.model.cliente = {
+                         id: '',
+                         Nombre: '',
+                         Direccion: '',
+                         Telefono: '',
+                         RFC: ''
+                        }
+                        this.mensaje = 1; //para mostrar mensaje de exito
+                    }
+                })
             }
         }
     }
