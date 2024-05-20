01ad0040201fe48df4a7e4e8a1a7525d34bb9a5a
Cesar Jesus Valenzuela Berrelleza <129009928+valenzuela03@users.noreply.github.com>
2024-05-20 11:02:52 -0700

    validas campos

--- tienda/src/views/ClientesCreateView.vue
+++ tienda/src/views/ClientesCreateView.vue
@@ -2,74 +2,98 @@
     <div class="container mt-5">
         <div class="card">
             <h4>Agregar clientes</h4>
-             <div v-if="mensaje == 1" class="alert alert-success" role="alert">  <!-- MENSAJE DE EXITO -->
+            <div v-if="mensaje == 1" class="alert alert-success" role="alert"> <!-- MENSAJE DE EXITO -->
                 Datos guardados con exito
             </div>
         </div>
         <div class="card-body">
-            <div class="mb-3">
-                Id
-                <input type="text" class="form-control" v-model="model.cliente.id">
-            </div>
-            <div class="mb-3">
-                Nombre
-                <input type="text" class="form-control" v-model="model.cliente.nombre">
-            </div>
-            <div class="mb-3">
-                Direccion
-                <input type="text" class="form-control" v-model="model.cliente.direccion">
-            </div>
-            <div class="mb-3">
-                Telefono
-                <input type="text" class="form-control" v-model="model.cliente.telefono">
-            </div>
-            <div class="mb-3">
-                RFC
-                <input type="text" class="form-control" v-model="model.cliente.rfc">
-            </div>
-            <div class="mb-3">
-                <button class="btn btn-primary" @click="guardarClientes()">Guardar</button>
-            </div>
+            <Form :validation-schema="validationSchema" @submit="onTodoBien"> 
+                <div class="mb-3">
+                    Id
+                    <Field name="id" id="id" type="text" class="form-control" v-model="model.cliente.id" />
+                    <ErrorMessage name="id" />
+                </div>
+                <div class="mb-3">
+                    Nombre
+                    <Field name="nombre" id="nombre" type="text" class="form-control" v-model="model.cliente.nombre" />
+                    <ErrorMessage name="nombre" />
+                </div>
+                <div class="mb-3">
+                    Direccion
+                    <Field name="direccion" id="direccion" type="text" class="form-control" v-model="model.cliente.direccion" />
+                    <ErrorMessage name="direccion" />
+                </div>
+                <div class="mb-3">
+                    Telefono
+                    <Field name="telefono" id="telefono" type="text" class="form-control" v-model="model.cliente.telefono" />
+                    <ErrorMessage name="telefono" />
+                </div>
+                <div class="mb-3">
+                    RFC
+                    <Field name="rfc" id="rfc" type="text" class="form-control" v-model="model.cliente.rfc" />
+                    <ErrorMessage name="rfc" />
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+            </Form>
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-    export default {
-        name: "ClientesCreate",
-        data(){
-            return{
-                mensaje: 0, //para mostrar mensaje de exito
-                model:{
-                    cliente:{
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import * as zod from 'zod';
+export default {
+    name: "ClientesCreate",
+    components: { Field, Form, ErrorMessage },
+    data() {
+        const validationSchema = toTypedSchema(
+            zod.object({
+                id: zod.string({message: 'Requerido'}).min(1),
+                nombre: zod.string({message: 'Requerido'}).min(1),
+                direccion: zod.string({message: 'Requerido'}).min(1),
+                telefono: zod.string({message: 'Requerido'}).min(1).max(10),
+                rfc: zod.string({message: 'Requerido'}).min(1),
+            })
+        )
+        return {
+            validationSchema,
+            mensaje: 0, //para mostrar mensaje de exito
+            model: {
+                cliente: {
                     id: '',
                     Nombre: '',
                     Direccion: '',
                     Telefono: '',
                     RFC: ''
                 }
-                }
             }
+        }
+    },
+    methods: {
+        onTodoBien(){
+            alert('Todo validado');
         },
-        methods:{
-            guardarClientes(){
-                axios.post('http://localhost:3000/api/clientes', this.model.cliente)
+        guardarClientes() {
+            axios.post('http://localhost:3000/api/clientes', this.model.cliente)
                 .then(res => {
                     //si insertamos 1 registro
-                    if(res.data.affectedRows == 1){ 
+                    if (res.data.affectedRows == 1) {
                         //limpiamos los cuadros de texto
                         this.model.cliente = {
-                         id: '',
-                         Nombre: '',
-                         Direccion: '',
-                         Telefono: '',
-                         RFC: ''
+                            id: '',
+                            Nombre: '',
+                            Direccion: '',
+                            Telefono: '',
+                            RFC: ''
                         }
                         this.mensaje = 1; //para mostrar mensaje de exito
                     }
                 })
-            }
         }
     }
+}
 </script>
